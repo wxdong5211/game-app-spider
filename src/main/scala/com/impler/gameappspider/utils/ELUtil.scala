478031e96a6c25681949a088f28d1098e2aab8da
Invalid <wxdong5211@gmail.com>
2015-01-07 12:42:00 +0800

    fix 正则转义分支过多 替换为异常处理此条抛弃数据

--- src/main/scala/com/impler/gameappspider/utils/ELUtil.scala
+++ src/main/scala/com/impler/gameappspider/utils/ELUtil.scala
@@ -4,11 +4,14 @@ import java.net.URLEncoder
 import java.util.regex.{Matcher, Pattern}
 
 import com.alibaba.fastjson.{JSONPath, JSONObject}
+import org.apache.logging.log4j.{LogManager, Logger}
 
 /**
  * ELUtil
  */
 object ELUtil {
+
+  private final val logger: Logger= LogManager.getLogger(getClass)
 
   val pattern: Pattern = Pattern.compile("\\$\\{([^\\}]+)\\}")
 
@@ -19,67 +22,56 @@ object ELUtil {
     if(!org.contains("$")){
       return org
     }
-    var str: String = org
-    var m: Matcher = pattern.matcher(str)
-    var g: String = null
-    var r: String = null
-    while (m.find()) {
-      g = m.group(1)
-      if(g.contains(":")){
-        val gs = g.split(":")
-        if(gs.length>1){
-          gs(0) match{
-            case "url" =>
-              r = contextString(gs(1),context)
-              r = URLEncoder.encode(r,"UTF-8")
-            case "sql" =>
-              r = contextString(gs(1),context)
-              r = r.replaceAll("'","''")
-            case "time" => if(gs(1)=="ms") r = ""+System.currentTimeMillis()
+    try{
+      var str: String = org
+      var m: Matcher = pattern.matcher(str)
+      var g: String = null
+      var r: String = null
+      while (m.find()) {
+        g = m.group(1)
+        if(g.contains(":")){
+          val gs = g.split(":")
+          if(gs.length>1){
+            gs(0) match{
+              case "url" =>
+                r = contextString(gs(1),context)
+                r = URLEncoder.encode(r,"UTF-8")
+              case "sql" =>
+                r = contextString(gs(1),context)
+                r = r.replaceAll("'","''")
+              case "time" => if(gs(1)=="ms") r = ""+System.currentTimeMillis()
+            }
+          }else{
+            r = contextString(g,context)
           }
         }else{
           r = contextString(g,context)
         }
-      }else{
-        r = contextString(g,context)
-      }
 
-      if(r==null||r.length()==0){
-        r = ""
+        if(r==null||r.length()==0){
+          r = ""
+        }
+        r = adapterRegSpecChar(r)
+        if(r.contains("\n")){
+          r=r.replaceAll("\n", "")
+        }
+        str = m.replaceFirst(r)
+        m = pattern.matcher(str)
       }
-      r = adapterRegSpecChar(r)
-      if(r.contains("\n")){
-        r=r.replaceAll("\n", "")
-      }
-      str = m.replaceFirst(r)
-      m = pattern.matcher(str)
+      str
+    }catch {
+      case any: Throwable =>
+        logger.error("org = "+org+",context = "+context,any)
+        ""
     }
-    str
+
   }
 
   def adapterRegSpecChar(org: String): String ={
     var dest = org
     // . \ + * ? [ ^ ] $ ( ) { } = ! < > | : -
-    dest = adapterRegSpecChar(dest,".")
     dest = adapterRegSpecChar(dest,"\\")
-    dest = adapterRegSpecChar(dest,"+")
-    dest = adapterRegSpecChar(dest,"*")
-    dest = adapterRegSpecChar(dest,"?")
-    dest = adapterRegSpecChar(dest,"[")
-    dest = adapterRegSpecChar(dest,"^")
-    dest = adapterRegSpecChar(dest,"]")
     dest = adapterRegSpecChar(dest,"$")
-    dest = adapterRegSpecChar(dest,"(")
-    dest = adapterRegSpecChar(dest,")")
-    dest = adapterRegSpecChar(dest,"{")
-    dest = adapterRegSpecChar(dest,"}")
-    dest = adapterRegSpecChar(dest,"=")
-    dest = adapterRegSpecChar(dest,"!")
-    dest = adapterRegSpecChar(dest,"<")
-    dest = adapterRegSpecChar(dest,">")
-    dest = adapterRegSpecChar(dest,"|")
-    dest = adapterRegSpecChar(dest,":")
-    dest = adapterRegSpecChar(dest,"-")
     dest
   }
 
